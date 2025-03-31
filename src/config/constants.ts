310c2050d983e2e636468fff1c45880cc831a8a3
Vibhanshu Garg <64217477+vibhanshu2001@users.noreply.github.com>
2025-03-31 17:19:43 +0530

    Update constants.ts

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -3,6 +3,6 @@ export const SUMMARIZE_LENGTH = 30;
 export const SCORE_THRESHOLD=0.7;
 export const NEARBY_RADIUS=100000;
 export const PORT=2056;
-export const GOOGLE_API_KEY=process.env.GOOGLE_API_KEY || "AIzaSyCJ_mArudNaCqD1Vjx44iawORKaUEZEYW4";
+export const GOOGLE_API_KEY=process.env.GOOGLE_API_KEY || "";
 export const GOOGLE_API_URL="https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent";
 export const ES_URL="http://elasticsearch:9200";
